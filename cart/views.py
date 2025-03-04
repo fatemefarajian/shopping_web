2963edb5dd2a4dd3acc6c01c0028506e29beb644
fateme farajian <fatemefarajian1374@gmail.com>
2025-03-04 12:56:05 +0330

    Add 'Add to Cart' button with AJAX functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
-# Create your views here.
+from shop.models import Product
+from .cart import Cart
+
+@require_POST
+def add_to_cart(request, product_id):
+    try:
+        product = get_object_or_404(Product, id=product_id)
+        cart = Cart(request)
+
+        cart.add(product)
+
+        context = {
+            'item_count': len(cart),
+            'total_price': cart.get_total_price(),
+        }
+
+        return JsonResponse(context)
+
+    except:
+        return JsonResponse({"error": "Something went wrong"})
