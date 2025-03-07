0ec6aa9120ad175e334fd2f0c26861963df790a8
fateme farajian <fatemefarajian1374@gmail.com>
2025-03-07 07:53:21 +0330

    Add 'add button' and 'decrease button' in cart using AJAX

--- cart/views.py
+++ cart/views.py
@@ -28,3 +28,35 @@ def add_to_cart(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     return render(request, "cart/cart_detail.html", {"cart": cart})
+
+
+@require_POST
+def update_quantity(request):
+    action = request.POST['action']
+    product_id = request.POST['product_id']
+
+    try:
+        product = get_object_or_404(Product, id=product_id)
+        cart = Cart(request)
+
+        if action == "add":
+            cart.add(product)
+        elif action == "decrease":
+            cart.decrease(product)
+
+        product_cost = cart.cart[product_id]['quantity'] * cart.cart[product_id]['price']
+
+        context = {
+            "success": True,
+            'item_count': len(cart),
+            'total_price': cart.get_total_price(),
+            'quantity': cart.cart[product_id]['quantity'],
+            'product_cost': product_cost,
+            'post_price': cart.get_post_price(),
+            'final_price': cart.get_final_price(),
+        }
+
+        return JsonResponse(context)
+
+    except:
+        return JsonResponse({"success": False, "error": "Something went wrong: product not found!"})
