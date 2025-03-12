7b935228e9347959b1eb004d44f6120689195293
fateme farajian <fatemefarajian1374@gmail.com>
2025-03-12 11:39:44 +0330

    Added validation method for custom user.

--- account/forms.py
+++ account/forms.py
@@ -8,8 +8,34 @@ class ShopUserCreationForm(UserCreationForm):
         model = ShopUser
         fields = ('phone', 'first_name', 'last_name', 'address', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
 
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if ShopUser.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('Phone number is already in exist.')
+        if not phone.isdigit():
+            raise forms.ValidationError('Phone must be digit.')
+        if not phone.startswith('09'):
+            raise forms.ValidationError('Phone must start with "09".')
+        if len(phone) != 11:
+            raise forms.ValidationError('Phone must have 11 digits.')
+
+        return phone
+
 
 class ShopUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = ShopUser
         fields = ('phone', 'first_name', 'last_name', 'address', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if ShopUser.objects.filter(phone=phone).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError('Phone number is already in exist.')
+        if not phone.isdigit():
+            raise forms.ValidationError('Phone must be digit.')
+        if not phone.startswith('09'):
+            raise forms.ValidationError('Phone must start with "09".')
+        if len(phone) != 11:
+            raise forms.ValidationError('Phone must have 11 digits.')
+
+        return phone
